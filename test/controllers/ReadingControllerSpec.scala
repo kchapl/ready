65a97e73bdc65c8557ec5716a275237d4fcb5076
K <kchapl@users.noreply.github.com>
2024-04-11 20:43:56 +0100

    Fix tests

--- test/controllers/ReadingControllerSpec.scala
+++ test/controllers/ReadingControllerSpec.scala
@@ -1,19 +1,37 @@
 package controllers
 
 import load.AppComponents
+import models.{Book, Reading}
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.mockito.MockitoSugar.*
 import org.scalatestplus.play.*
 import org.scalatestplus.play.components.OneAppPerSuiteWithComponents
 import play.api.test.*
 import play.api.test.Helpers.*
+import services.ReadingStorage
 
-class ReadingControllerSpec extends PlaySpec with OneAppPerSuiteWithComponents:
+import java.time.LocalDate
+
+class ReadingControllerSpec extends PlaySpec with OneAppPerSuiteWithComponents with MockitoSugar:
 
   override val components: AppComponents = new AppComponents(context)
 
   "showAll" should {
 
     "render the readingAll page from a new instance of controller" in {
-      val controller = new ReadingController(stubControllerComponents())
+      val readingStorage = mock[ReadingStorage]
+      when(readingStorage.fetchAll()).thenReturn(
+        Seq(
+          Reading.Completed(
+            book = Book(isbn = "123", author = "a b", title = "t1"),
+            started = LocalDate.of(2023, 1, 2),
+            completed = LocalDate.of(2023, 3, 12),
+            rating = 3
+          )
+        )
+      )
+      val controller = new ReadingController(stubControllerComponents(), readingStorage)
       val home = controller.showAll().apply(FakeRequest(GET, "/readings"))
 
       status(home) mustBe OK
